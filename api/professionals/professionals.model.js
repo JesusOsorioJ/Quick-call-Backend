865a1c7aa8dcc95e46420b5bd41bc202aa84cae6
Luis Salcedo Salas <rideluis@gmail.com>
2022-03-25 13:07:59 -0500

    feat: professionals model

--- api/professionals/professionals.model.js
+++ api/professionals/professionals.model.js
@@ -1,6 +1,62 @@
 const moongose = require('mongoose');
 
-const ProSchema = new mongoose.Schema(
+const specialty = new mongoose.Schema(
+    {
+        certified: {
+            type: String,
+            required: true
+        },
+        inProgress: {
+            type: String,
+            required: true
+        },
+        nonCertified: {
+            type: String,
+            required: true
+        }
+    }
+);
+
+const image = new mongoose.Schema(
+    {
+        profile: {
+            data: Buffer,
+            contentType: String
+        },
+        myJobs: {
+            data: Buffer,
+            contentType: String
+        }
+    }
+);
+
+const socialSecurity = new mongoose.Schema (
+    {
+        eps: {
+            type: String,
+            required: true
+        },
+        arl: {
+            type: String,
+            required: true
+        }
+    }
+);
+
+const availability = new mongoose.Schema(
+    {
+        schedule: {
+            type: String,
+            required: true
+        },
+        fullAvailability: {
+            type: Boolean,
+            required: true
+        }
+    }
+);
+
+const professionalSchema = new mongoose.Schema(
     {
         name: {
             type: String,
@@ -15,24 +71,39 @@ const ProSchema = new mongoose.Schema(
             type: String,
             required: true,
             trim: true
-        }
+        },
         phoneNumber: {
             type: Number,
             trim: true,
-        }
+        },
         city: {
             type: String,
             trim: true
-        }
+        },
         myDescription: {
             type: String,
             required: true,
             trim: true
+        },
+        specialty: {
+            type: [specialty],
+            default: null
+        },
+        image: {
+            type: image,
+            default: null
+        },
+        socialSecurity: {
+            type: socialSecurity,
+            default: null
+        },
+        availability: {
+            type: availability,
+            default: null
         }
-    }
-)
+    },
+    { timestamps: true },
+    { versionKey: false }
+);
 
-const Specialty = new mongoose.Schema(
-    certified
-
-)
+module.exports = new mongoose.model('Professional', professionalSchema);
