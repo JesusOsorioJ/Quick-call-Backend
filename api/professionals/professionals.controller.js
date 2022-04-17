97085d4efe3e110dc529262986fced6ea594ed35
jesus osorio <jesdaos@hotmail.com>
2022-04-17 09:30:16 -0500

    adding multer cluodinary

--- api/professionals/professionals.controller.js
+++ api/professionals/professionals.controller.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const cloudinary = require('cloudinary').v2;
+
 const { AllProfessionals,
   OneProfessional,
   CreateProfessional,
@@ -7,6 +10,17 @@ const { AllProfessionals,
 async function handlerAllProfessionals(req, res) {
   const professional = await AllProfessionals(req.query);
   return res.json(professional);
+}
+
+async function uploadImage(image) {
+  try {
+    const result = await cloudinary.uploader.upload(image);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally{
+    fs.unlinkSync(image);
+  }
 }
 
 async function handlerOneProfessional(req, res) {
@@ -20,9 +34,29 @@ async function handlerOneProfessional(req, res) {
 }
 
 async function handlerCreateProfessional(req, res) {
-  const newProfessional = req.body;
-  const profesional = await CreateProfessional(newProfessional);
-  return res.status(201).json(profesional);
+
+  const  data  = req.body;
+  console.log("xxxxxxxxxxxxxxxxxxxxx filles", data)
+
+  // for (let step = 0; step < specialty.length ; step++) {
+  //   past = specialty[step]
+  //   image = req[past];
+  //   if (image) {
+  //     await uploadImage(image.path);
+  //   }
+  // }
+
+  // specialty.map((special) => (
+  //   image = req[special];
+  //   if (image) {
+  //     await uploadImage(image.path);
+  //   }
+
+  //   ));
+
+  // const newProfessional = req.body;
+  // const profesional = await CreateProfessional(newProfessional);
+  // return res.status(201).json(profesional);
 }
 
 async function handlerEditProfessional(req, res) {
