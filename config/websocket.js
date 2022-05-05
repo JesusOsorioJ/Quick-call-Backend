c8b812ae7254a982a0c61a7ff9320802d810d0be
Luis Salcedo Salas <rideluis@gmail.com>
2022-05-05 11:26:25 -0500

    feat: add quick call as authorized CORS

--- config/websocket.js
+++ config/websocket.js
@@ -3,7 +3,7 @@ const socket = {};
 
 function connectSocket(server) {
     const options = {
-        cors: { origin: ['*', 'http://localhost:8080', 'http://localhost:3000'] }
+        cors: { origin: ['*', 'http://localhost:8080', 'http://localhost:3000', 'https://quickcall.netlify.app'] }
     };
     const io = new socketio.Server(server, options);
 
